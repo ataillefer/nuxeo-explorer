217c874db5b2bbeb3affa9022d0e38c0f068e213
Anahide Tchertchian <atchertchian@nuxeo.com>
2020-11-12 09:16:21 +0100

    NXP-29844: temp remove of notifs (REMOVE BEFORE MERGE)

--- ci/Jenkinsfiles/export.groovy
+++ ci/Jenkinsfiles/export.groovy
@@ -315,21 +315,4 @@ pipeline {
     }
   }
 
-  post {
-    success {
-      script {
-        if (env.UPLOAD_EXPORT == "true" && !hudson.model.Result.SUCCESS.toString().equals(currentBuild.getPreviousBuild()?.getResult())) {
-          slackSend(channel: "${SLACK_CHANNEL}", color: "good", message: "Successfully <${BUILD_URL}|uploaded> nuxeo-explorer reference export for ${NUXEO_IMAGE_VERSION} :robot_face:")
-        }
-      }
-    }
-    unsuccessful {
-      script {
-        if (env.UPLOAD_EXPORT == "true") {
-          slackSend(channel: "${SLACK_CHANNEL}", color: "danger", message: "Failed to <${BUILD_URL}|upload> nuxeo-explorer reference export for ${NUXEO_IMAGE_VERSION}")
-        }
-      }
-    }
-  }
-
 }
